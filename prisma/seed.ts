6b62d00a68f16db23c2ddb7c4079472c5ca1ede4
alikiriza carol <alikirizacarol7@gmail.com>
2026-09-16 16:38:17 +0300

    the

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,9 @@
 import { PrismaClient } from "../lib/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const db = new PrismaClient();
+const db = new PrismaClient({
+  adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL! }),
+});
 
 const categories = [
   { name: "Face Care", slug: "face-care", description: "Nourishing facial skincare for a radiant glow", image: "/images/face-care.jpg" },
